20eb05050bb30dfe5c755065ca5adab01b178ab1
MikeTheFoxFromDream <93942359+MikeTheFoxFromDream@users.noreply.github.com>
2023-12-22 13:16:11 +0100

    Stepper hopefully ok

--- WebServer/main.py
+++ WebServer/main.py
@@ -4,7 +4,6 @@ import time
 import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-
 class RequestHandler(BaseHTTPRequestHandler):
 
     # Prozatím jen pro testování funkčnosti avšak v budoucnu se bude možná zde aplikace rozvíjet
@@ -33,18 +32,46 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 
 def passNextBall(delay):
-
     time.sleep(delay)
+    #set DIP pins on S1=ON, S2=OFF, S3=ON
+    for x in range(1, 201):
+        GPIO.output(EN, GPIO.HIGH)
+        GPIO.output(PUL, GPIO.HIGH)
+        GPIO.output(PUL, GPIO.LOW)
+    return
+
+def aimServo():
+    return
+
+def changeMotorSpeed():
     return
 
 def startMode(data):
+    global nextDelay
     jsonData = json.loads(data)
     for ball in jsonData["micky"].items():
         temp = str(ball[1]).replace("\'", "\"")
         ballData = json.loads(temp)
-        passNextBall(int(ballData["delay"]))
+        passNextBall(nextDelay)
+        nextDelay = int(ballData["delay"])
     return
-GPIO.setup(13, GPIO.OUT)
+
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+
+nextDelay = 0
+PUL = 6
+DIR = 13
+EN = 12
+SERVO = 14
+
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(PUL, GPIO.OUT)
+GPIO.setup(EN, GPIO.OUT)
+GPIO.setup(SERVO, GPIO.OUT)
+
+servo = GPIO.PWM(SERVO, 50)
 
 server = HTTPServer(("", 8000), RequestHandler)
 server.serve_forever()
